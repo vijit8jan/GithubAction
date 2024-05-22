94bfbe6041dce815ce6ef7c5fcad70e43e1bdf57
shekharbo <shekharborse1@gmail.com>
2024-05-22 11:37:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 781365762844.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 584294315145.dkr.ecr.us-east-1.amazonaws.com
 
-docker rmi -f 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
-
-docker pull 781365762844.dkr.ecr.us-east-1.amazonaws.com/githubaction-poc:latest
+docker rmi -f 584294315145.dkr.ecr.us-east-1.amazonaws.com/github-poc:latest
+docker pull 584294315145.dkr.ecr.us-east-1.amazonaws.com/github-poc:latest
